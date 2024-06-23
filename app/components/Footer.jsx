b350e837e98b5fcbd8d1e5ee637fbfb35e46acef
devadg <opponamer@gmail.com>
2024-06-23 22:32:53 +0100

    cool

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link"
-import NavLogo from '@/public/heroimg.png'
 import { FaFacebookF, FaInstagram, FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 export default function Footer() {
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import NavLogo from '@/public/heroimg.png'
 import { HiMenuAlt1 } from "react-icons/hi";
 import { FiSearch } from "react-icons/fi";
 
