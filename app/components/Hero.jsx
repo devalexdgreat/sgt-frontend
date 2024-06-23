f29eff643c7d951fc1dc8fab8cb0fc835f5f0642
devadg <opponamer@gmail.com>
2024-06-23 22:43:00 +0100

    cool

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,4 +1,3 @@
-import heroimg from '@/public/heroimg.png'
 import Image from 'next/image';
 import Link from 'next/link';
 import SignupForm from './SignupForm';
@@ -8,7 +7,7 @@ export default function Hero() {
         <div className="w-full bg-black text-white h-screen flex justify-center items-center pt-16">
             <div className="w-11/12 mx-auto bg-black flex md:justify-between h-5/6 justify-center items-center">
                 <div className='flex flex-col items-center text-center md:text-left md:items-start'>
-                    <Image src={heroimg} className='w-full' alt='' height={100} width={100} />
+                    <Image src='/heroimg.png' className='w-full' alt='' height={100} width={100} />
                     <p className='py-8 text-md'>Show case your talent and stand a chance to win big.
                     <br className='hidden md:block'/> Will you be the next street champion?</p>
                     <div>
